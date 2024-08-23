b0bb33661917de165c3ac593fb114dddffe21be8
max <max@max.com>
2024-08-23 15:22:48 +0300

    eighth commit

--- src/data.js
+++ src/data.js
@@ -1,151 +1,161 @@
 // Import images
 import image1 from './images/klaxcar_double.jpeg';
-import image2 from './images/minilight_2eye.jpeg';
+//import image2 from './images/minilight_2eye.jpeg';
 import image3 from './images/minilight.jpeg';
 import image4 from './images/4_eye.png';
 import image5 from './images/2-eye_policelight.jpeg'
-import image6 from './images/brake_step.jpeg';
+//import image6 from './images/brake_step.jpeg';
 import image7 from './images/brake_light.jpeg';
-import image8 from './images/globe.jpeg';
-import image9 from './images/headlight_lamp.jpeg';
-import image10 from './images/arrow.jpg';
-import image11 from './images/h1_zinno.jpg';
-import image12 from './images/h3_zinno.jpg';
-import image13 from './images/parking_bulb.jpeg';
-import image14 from './images/h11_zinno.jpg';
-import image15 from './images/radiator_cup.jpg';
-import image16 from './images/press_switch.jpg';
-import image17 from './images/tube.jpg';
+//import image8 from './images/globe.jpeg';
+//import image9 from './images/headlight_lamp.jpeg';
+//import image10 from './images/arrow.jpg';
+//import image11 from './images/h1_zinno.jpg';
+//import image12 from './images/h3_zinno.jpg';
+//import image13 from './images/parking_bulb.jpeg';
+//import image14 from './images/h11_zinno.jpg';
+//import image15 from './images/radiator_cup.jpg';
+//import image16 from './images/press_switch.jpg';
+import image17 from './images/on_off.jpeg';
 import image18 from './images/tube.jpg';
-import image19 from './images/compat_twin_horn.jpeg';
-import image20 from './images/mini_sidelight.png';
-import image21 from './images/spotlight.png';
+//import image19 from './images/compat_twin_horn.jpeg';
+//import image20 from './images/mini_sidelight.png';
+import image21 from './images/360_led_light.jpeg';
+import image22 from './images/denso_horn.jpeg';
+import image23 from './images/t20_single.jpeg';
+import image24 from './images/strobes.jpeg';
+//import image25 from './images/klaxcar_double.jpeg';
+import image26 from './images/normal_parking_bulb.jpeg';
+import image27 from './images/golden.jpeg';
+import image28 from './images/fuse.jpeg';
+//import image29 from './images/klaxcar_double.jpeg';
+//import image30 from './images/klaxcar_double.jpeg';
+
 export const items = [
   {
     id: 1,
-    name: "klaxcar_double 12v (3 pkts)",
+    name: "klaxcar_double 12v (5 pcs)",
     image: image1,  // Use the imported image
-    description: "klaxcar_double.",
-    quantity: 3,  // Initial quantity
-  },
-  {
-    id: 2,
-    name: "minilight_2eye (10 pcs)",
-    image: image2,  // Use the imported image
-    description: "minilight_2eye.",
-    quantity: 10,  // Initial quantity
-  },
-   {
-    id: 3,
-    name: "minilight (10 pcs)",
-    image: image3,  // Use the imported image
-    description: "minilight.",
-    quantity: 10,  // Initial quantity
-  },
-   {
-    id: 4,
-    name: "brake_light_rgb(10 pcs)",
-    image: image7,  // Use the imported image
-    description: "brake_light.",
-    quantity: 10,  // Initial quantity
-  },
-   {
-    id: 5,
-    name: "2-eye_policelight (8 pcs)",
-    image: image5,  // Use the imported image
-    description: "2-eye_policelight.",
-    quantity: 8,  // Initial quantity
-  },
-   {
-    id: 6,
-    name: "brake step (7 pcs)",
-    image: image6,  // Use the imported image
-    description: "brake step",
-    quantity: 7,  // Initial quantity
-  },
-   {
-    id: 7,
-    name: "brake_light red(20 pcs)",
-    image: image7,  // Use the imported image
-    description: "brake_light red.",
-    quantity: 20,  // Initial quantity
-  },
-  {
-    id: 8,
-    name: "Globe tape (15 pcs) ",
-    image: image8,  // Use the imported image
-    description: "Globe tape.",
-    quantity: 15,  // Initial quantity
-  },
-  {
-    id: 9,
-    name: "headlight_lamp (5 pcs)",
-    image: image9,  // Use the imported image
-    description: "headlight_lamp.",
+    description: "klaxcar_double. 12v",
     quantity: 5,  // Initial quantity
   },
   {
+    id: 2,
+    name: "klaxcar double 24v (5 pcs)",
+    image: image1,  // Use the imported image
+    description: "klaxcar double 24v",
+    quantity: 5,  // Initial quantity
+  },
+   {
+    id: 3,
+    name: "klaxcar single 12v (2 pcs)",
+    image: image1,  // Use the imported image
+    description: "klaxcar single 12v.",
+    quantity: 2,  // Initial quantity
+  },
+   {
+    id: 4,
+    name: "klaxcar single 24v (2 pcs)",
+    image: image1,  // Use the imported image
+    description: "klaxcar single 24v.",
+    quantity: 2,  // Initial quantity
+  },
+   {
+    id: 5,
+    name: "2-eye_policelight (5 pcs)",
+    image: image5,  // Use the imported image
+    description: "2-eye_policelight.",
+    quantity: 5,  // Initial quantity
+  },
+   {
+    id: 6,
+    name: "minilight (4 pcs)",
+    image: image3,  // Use the imported image
+    description: "minilight",
+    quantity: 4,  // Initial quantity
+  },
+   {
+    id: 7,
+    name: "brake_light red(10 pcs)",
+    image: image7,  // Use the imported image
+    description: "brake_light red.",
+    quantity: 10,  // Initial quantity
+  },
+  {
+    id: 8,
+    name: "Denso (2 pcs)",
+    image: image22,  // Use the imported image
+    description: "denso.",
+    quantity: 2,  // Initial quantity
+  },
+  {
+    id: 9,
+    name: "360 light (4 pcs)",
+    image: image21,  // Use the imported image
+    description: "360 light",
+    quantity: 4,  // Initial quantity
+  },
+  {
     id: 10,
-    name: "arrow light yellow (8 pcs)",
-    image: image10,  // Use the imported image
-    description: "arrow light yellow",
-    quantity: 8,  // Initial quantity
+    name: "switch on/off (20 pcs)",
+    image: image17,  // Use the imported image
+    description: "switch on/off",
+    quantity: 20,  // Initial quantity
   },
   {
     id: 11,
-    name: "h1_zinno 12v(1 pkts)",
-    image: image11,  // Use the imported image
-    description: "h1_zinno 12v",
-    quantity: 1,  // Initial quantity
+    name: "switch on/on/off (20 pcs)",
+    image: image17,  // Use the imported image
+    description: "switch on/on/off",
+    quantity: 20,  // Initial quantity
   },
   {
     id: 12,
-    name: "h3_zinno 12v(1 pkt)",
-    image: image12,  // Use the imported image
-    description: "h3_zinno 12v",
-    quantity: 1,  // Initial quantity
+    name: "koito single 12v (4 pcs )",
+    image: image23,  // Use the imported image
+    description: "koito single 12v",
+    quantity: 4,  // Initial quantity
   },
   {
     id: 13,
-    name: "parking_bulb blue (10 pcs)",
-    image: image13,  // Use the imported image
-    description: "parking_bulb.",
-    quantity: 10,  // Initial quantity
+    name: "koito double 12v (2 pcs)",
+    image: image23,  // Use the imported image
+    description: "koito double 12v",
+    quantity: 2,  // Initial quantity
   },
   {
     id: 14,
-    name: "h11_zinno 12v(1 pkt)",
-    image: image14,  // Use the imported image
-    description: "h11_zinno 12v.",
-    quantity: 1,  // Initial quantity
+    name: "rock single 12v (3 pcs)",
+    image: image26,  // Use the imported image
+    description: "rock single 12v.",
+    quantity: 3,  // Initial quantity
   },
   {
     id: 15,
-    name: "radiator_cup (1 strip)",
-    image: image15,  // Use the imported image
-    description: "radiator_cup.",
-    quantity: 1,  // Initial quantity
+    name: "rock single 24v (3 pcs)",
+    image: image26,  // Use the imported image
+    description: "rock single 24v.",
+    quantity: 3,  // Initial quantity
   },
   {
     id: 16,
-    name: "press_switch (10 pcs)",
-    image: image16,  // Use the imported image
-    description: "press_switch.",
-    quantity: 10,  // Initial quantity
+    name: "rock double 12v (5 pcs)",
+    image: image26,  // Use the imported image
+    description: "rock double 12v.",
+    quantity: 5,  // Initial quantity
   },
   {
     id: 17,
-    name: "tube za yellow (10 pcs)",
-    image: image17,  // Use the imported image
-    description: "tube za yellow.",
-    quantity: 10,  // Initial quantity
+    name: "rock double 24v (3 pcs)",
+    image: image26,  // Use the imported image
+    description: "rock double 24v.",
+    quantity: 3,  // Initial quantity
   },
   {
     id: 18,
-    name: "tube za green (10 pcs)",
-    image: image18,  // Use the imported image
-    description: "tube za green.",
-    quantity: 10,  // Initial quantity
+    name: "golden spotlight (3 pcs)",
+    image: image27,  // Use the imported image
+    description: "golden spotlight.",
+    quantity: 3,  // Initial quantity
   },
   {
     id: 19,
@@ -156,64 +166,58 @@ export const items = [
   },
   {
     id: 20,
-    name: "compat_twin_horn (2 pcs)",
-    image: image19,  // Use the imported image
-    description: "compat_twin_horn.",
-    quantity: 2,  // Initial quantity
+    name: "strobes red (5 pcs)",
+    image: image24,  // Use the imported image
+    description: "strobes red.",
+    quantity: 5,  // Initial quantity
   },
   {
     id: 21,
-    name: "minilight_sidelight (10 pcs)",
-    image: image20,  // Use the imported image
-    description: "minilight_sidelight.",
-    quantity: 10,  // Initial quantity
+    name: "strobes blue (5 pcs)",
+    image: image24,  // Use the imported image
+    description: "strobes blue.",
+    quantity: 5,  // Initial quantity 
   },
   {
     id: 22,
-    name: "spotlight (4 pcs)",
-    image: image21,  // Use the imported image
-    description: "compat_twin_horn.",
-    quantity: 4,  // Initial quantity
+    name: "strobes green (2 pcs)",
+    image: image24,  // Use the imported image
+    description: "strobes green.",
+    quantity: 2,  // Initial quantity
   },
-	{
+  {
     id: 23,
     name: "4 eye light ( 6 pcs)",
     image: image4,  // Use the imported image
     description: "4 eye light.",
     quantity: 6,  // Initial quantity
   },
+  {
+    id: 24,
+    name: "fuse 10A (30 pcs)",
+    image: image28,
+    description: "fuse 10A",
+    quantity: 30,
+  },
+  {
+    id: 25,
+    name: "fuse 30A (20 pcs)",
+    image: image28,  // Use the imported image
+    description: "fuse 30A.",
+    quantity: 20,  // Initial quantity
+  },
   // Add more items as needed
 ];
 export const otherItems = [
   {
-    id: 24,
-    name: "h7 360 degrees led light(2 pcs)",
-    description: "h7 360 degrees led light.",
-  },
-  {
-    id: 25,
-    name: "H4B 12v (1 packet) ",
-    description: "H4B 12v",
-  },
-  {
     id: 26,
-    name: "h1, h3, h7 and h11 za koito 12 v (1 pkt each)",
-    description: "Koito product each",
-  },
-  {
-    id: 27,
-    name: "flasher (5 pcs)",
-    description: "flasher.",
-  },
-  {
-    id: 28,
     name: "Any other new thing in market",
     description: "Please add anything new if possible.",
   },
   {
-    id: 29,
+    id: 27,
     name: "Payment details",
-    description: "Paid 21,100 on 19/08/2024.",
+    description: "Paid 16,740 on 23/08/2024.",
   },
   // Add more items without images as needed
 ];
